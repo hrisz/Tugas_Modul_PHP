18af311b380d043a8d4cc2940d259d7e4fdbdcfd
hrisz <harissaefuloh@gmail.com>
2022-12-17 14:19:23 +0700

    modification_tugas1.php

--- tugas1.php
+++ tugas1.php
@@ -9,20 +9,14 @@
 <body>
     <h2>Latihan 1</h2>
     <?php
-    $ganjil = 7;
-    switch ($ganjil)
+    $angka = 7;
+    if ($angka % 2)
     {
-        case 0: echo("0 bukan bilangan genap maupun ganjil"); break;
-        case 1: echo("1 adalah bilangan ganjil"); break;
-        case 2: echo("2 bukan bilangan ganjil"); break;
-        case 3: echo("3 adalah bilangan ganjil"); break;
-        case 4: echo("4 bukan bilangan ganjil"); break;
-        case 5: echo("5 adalah bilangan ganjil"); break;
-        case 6: echo("6 bukan bilangan ganjil"); break;
-        case 7: echo("7 adalah bilangan ganjil"); break;
-        case 8: echo("8 bukan bilangan ganjil"); break;
-        case 9: echo("7 adalah bilangan ganjil"); break;
-        default: echo("Masukkan angka antara 1 - 9");
+        echo ("$angka adalah bilangan ganjil");
+    }
+    else
+    {
+        echo ("$angka adalah bilangan genap");
     }
     ?>
 </body>
